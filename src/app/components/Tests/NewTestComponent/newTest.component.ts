94e34900996ceab469e02110ca00a9e56fa2848f
Mind <mindkriauciunas@gmail.com>
2021-09-08 00:52:45 +0300

    QuestionForm. Points Increment/Decrement.

--- src/app/components/Tests/NewTestComponent/newTest.component.ts
+++ src/app/components/Tests/NewTestComponent/newTest.component.ts
@@ -1,11 +1,25 @@
-import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import {
+  FormArray,
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { first } from 'rxjs/operators';
 import { StateType } from 'src/app/models/testType';
 import * as moment from 'moment';
 import { ValidTime } from 'src/app/helpers/validTime.validator';
 import { DateSelectionModelChange } from '@angular/material/datepicker';
 import { HttpUploadProgressEvent } from '@angular/common/http';
+import { QuestionType } from 'src/app/models/questionType';
 
 @Component({
   templateUrl: 'newTest.component.html',
@@ -13,6 +27,8 @@ import { HttpUploadProgressEvent } from '@angular/common/http';
 })
 export class NewTestComponent implements OnInit {
   testForm: FormGroup;
+  questionForm: FormGroup;
+  questions: FormArray;
   submitted = false;
   loading = false;
   isCreateMode: boolean;
@@ -24,13 +40,23 @@ export class NewTestComponent implements OnInit {
     (value) => typeof value !== 'number'
   );
 
+  public QuestionEnum = Object.values(QuestionType).filter(
+    (value) => typeof value !== 'number'
+  );
+
   @ViewChild('hourInput') public hoursInputRef: ElementRef;
   @ViewChild('minutesInput') public minutesInputRef: ElementRef;
+  @ViewChildren('pointsInput') public pointsInputRef: QueryList<ElementRef>;
 
   @ViewChild('hoursDecrementBtn') public hoursDecrementBtn: ElementRef;
   @ViewChild('hoursIncrementBtn') public hoursIncrementBtn: ElementRef;
   @ViewChild('minutesDecrementBtn') public minutesDecrementBtn: ElementRef;
   @ViewChild('minutesIncrementBtn') public minutesIncrementBtn: ElementRef;
+
+  @ViewChildren('pointsDecrementBtn')
+  public pointsDecrementBtn: QueryList<ElementRef>;
+  @ViewChildren('pointsIncrementBtn')
+  public pointsIncrementBtn: QueryList<ElementRef>;
 
   constructor(private formBuilder: FormBuilder) {}
   ngOnInit() {
@@ -38,27 +64,31 @@ export class NewTestComponent implements OnInit {
     // this.isCreateMode = !this.id;
     // this.dialogTitle = this.data.dialogTitle;
     // this.confirmBtnTxt = this.data.confirmBtnTxt;
-
+    console.log(this.pointsIncrementBtn);
     this._setMinDate();
-
     this.testForm = this.formBuilder.group(
       {
-        name: ['', Validators.required],
-        description: ['', Validators.required],
-        enumSelect: [null, Validators.required],
-        dateFrom: [null, Validators.required],
-        dateTo: [null, Validators.required],
-        hourInput: [
-          Number,
-          [Validators.required, Validators.min(0), Validators.max(48)],
-        ],
-        minutesInput: [
-          Number,
-          [Validators.required, Validators.min(0), Validators.max(59)],
-        ],
+        name: new FormControl('', Validators.required),
+        description: new FormControl('', Validators.required),
+        enumSelect: new FormControl(null, Validators.required),
+        dateFrom: new FormControl(null, Validators.required),
+        dateTo: new FormControl(null, Validators.required),
+        hourInput: new FormControl(Number, [
+          Validators.required,
+          Validators.min(0),
+          Validators.max(48),
+        ]),
+        minutesInput: new FormControl(Number, [
+          Validators.required,
+          Validators.min(0),
+          Validators.max(59),
+        ]),
+        questions: this.formBuilder.array([]),
       },
       {
         validator: ValidTime('dateFrom', 'dateTo'),
+        //add custom validator to check if both time inputs are not 0.
+        //add custom validator to check if all question points sum up less than 100
       }
     );
     // if (!this.isCreateMode) {
@@ -81,6 +111,38 @@ export class NewTestComponent implements OnInit {
   get f() {
     return this.testForm.controls;
   }
+
+  get g() {
+    return this.questionForm.controls;
+  }
+
+  get Questions() {
+    return this.testForm.controls['questions'] as FormArray;
+  }
+
+  addQuestion() {
+    this.questionForm = this.formBuilder.group({
+      questionName: new FormControl('', Validators.required),
+      questionDescription: new FormControl('', Validators.required),
+      questionType: new FormControl(null, Validators.required),
+      worthOfPoints: new FormControl(1, [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(100),
+      ]),
+      // submittedSolution: new FormControl(null, Validators.required),
+    });
+
+    this.Questions.push(this.questionForm);
+  }
+
+  removeQuestion(i: number) {
+    this.Questions.removeAt(i);
+  }
+
+  // getControls() {
+  //   return (this.testForm.get('questions') as FormArray).controls;
+  // }
 
   onSubmit() {
     this.submitted = true;
@@ -158,34 +220,46 @@ export class NewTestComponent implements OnInit {
     //   });
   }
 
-  continuousIncrease(value: string) {
+  continuousIncrease(value: string, i?: number) {
     if (value === 'hours') {
       this.increment('hours');
       this.timeout = setInterval(() => {
         this.increment('hours');
-      }, 250);
+      }, 200);
       return false;
-    } else {
+    } else if (value === 'minutes') {
       this.increment('minutes');
       this.timeout = setInterval(() => {
         this.increment('minutes');
-      }, 250);
+      }, 200);
+      return false;
+    } else {
+      this.increment('points', i!);
+      this.timeout = setInterval(() => {
+        this.increment('points', i!);
+      }, 150);
       return false;
     }
   }
 
-  continuousDecrease(value: string) {
+  continuousDecrease(value: string, i?: number) {
     if (value === 'hours') {
       this.decrement('hours');
       this.timeout = setInterval(() => {
         this.decrement('hours');
-      }, 250);
+      }, 200);
       return false;
-    } else {
+    } else if (value === 'minutes') {
       this.decrement('minutes');
       this.timeout = setInterval(() => {
         this.decrement('minutes');
-      }, 250);
+      }, 200);
+      return false;
+    } else {
+      this.decrement('points', i!);
+      this.timeout = setInterval(() => {
+        this.decrement('points', i!);
+      }, 150);
       return false;
     }
   }
@@ -200,18 +274,25 @@ export class NewTestComponent implements OnInit {
     return false;
   }
 
-  increment(value: string) {
+  increment(value: string, i?: number) {
     if (value === 'hours') {
       this.hoursInputRef.nativeElement.stepUp();
-    } else {
+    } else if (value === 'minutes') {
       this.minutesInputRef.nativeElement.stepUp(5);
+    } else {
+      const ele = this.pointsInputRef.get(i!);
+      ele!.nativeElement.stepUp();
     }
   }
-  decrement(value: string) {
+
+  decrement(value: string, i?: number) {
     if (value === 'hours') {
       this.hoursInputRef.nativeElement.stepDown();
-    } else {
+    } else if (value === 'minutes') {
       this.minutesInputRef.nativeElement.stepDown(5);
+    } else {
+      const ele = this.pointsInputRef.get(i!);
+      ele!.nativeElement.stepDown();
     }
   }
 }
