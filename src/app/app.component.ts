8da1358e31b622a087407caf4c409cf24f59efbf
Mind <mindkriauciunas@gmail.com>
2021-09-25 00:44:54 +0300

    ScrollFix Final

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,11 @@ import { Router } from '@angular/router';
 import { User } from './models/user';
 import { AuthenticationService } from './services/authentication.service';
 import { fromEvent, Observable, Subscription } from 'rxjs';
-import { disableBodyScroll, enableBodyScroll } from 'body-scroll-lock';
+import {
+  clearAllBodyScrollLocks,
+  disableBodyScroll,
+  enableBodyScroll,
+} from 'body-scroll-lock';
 
 @Component({
   selector: 'app',
@@ -18,30 +22,41 @@ export class AppComponent {
   resizeObservable$: Observable<Event>;
   resizeSubscription$: Subscription;
 
-  @ViewChild('navBar') scrollTarget: ElementRef;
+  constructor(
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    this.authenticationService.currentUser.subscribe(
+      (x) => (this.currentUser = x)
+    );
+  }
 
-  ngOnInit() {
+  @ViewChild('navBar') navBar: ElementRef;
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    if (window.innerWidth > 768 && this.isOpen === true) {
+      this.isOpen = !this.isOpen;
+      enableBodyScroll(this.navBar.nativeElement);
+    } else if (window.innerWidth > 768 && this.isOpen === false) {
+      enableBodyScroll(this.navBar.nativeElement);
+    }
+
     this.resizeObservable$ = fromEvent(window, 'resize');
     this.resizeSubscription$ = this.resizeObservable$.subscribe((evt) => {
       if (window.innerWidth > 768 && this.isOpen === true) {
         this.isOpen = !this.isOpen;
-        enableBodyScroll(this.scrollTarget.nativeElement);
+        enableBodyScroll(this.navBar.nativeElement);
+      } else if (window.innerWidth > 768 && this.isOpen === false) {
+        enableBodyScroll(this.navBar.nativeElement);
       }
     });
   }
 
   ngOnDestroy() {
     this.resizeSubscription$.unsubscribe();
-  }
-
-  constructor(
-    private router: Router,
-    private authenticationService: AuthenticationService,
-    private elem: ElementRef
-  ) {
-    this.authenticationService.currentUser.subscribe(
-      (x) => (this.currentUser = x)
-    );
+    clearAllBodyScrollLocks();
   }
 
   logout() {
@@ -53,9 +68,15 @@ export class AppComponent {
   toggleCollapse() {
     this.isOpen = !this.isOpen;
     if (this.isOpen) {
-      disableBodyScroll(this.scrollTarget.nativeElement);
+      disableBodyScroll(this.navBar.nativeElement);
     } else {
-      enableBodyScroll(this.scrollTarget.nativeElement);
+      enableBodyScroll(this.navBar.nativeElement);
+    }
+  }
+
+  close() {
+    if (this.isOpen) {
+      this.isOpen = !this.isOpen;
     }
   }
 }
